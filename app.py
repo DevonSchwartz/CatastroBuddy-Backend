b51cf9f890dd1b40c92e2f683fd00f63f4dbfe3b
Devon Schwartz <devon.s.schwartz@gmail.com>
2024-06-27 09:47:34 -0500

    Added route to update an item

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def add_client_items(client_id):
     request_data = request.get_json()
     new_item = item.Item(request_data[Fields.itemName.name], request_data[Fields.description.name],
                             request_data[Fields.originalPhoto.name], request_data[Fields.price.name],
-                            request_data[Fields.damaged.name], request_data[Fields.damagedPhoto.name])
+                            request_data[Fields.damaged.name], request_data[Fields.damagedPhoto.name],
+                            None)
     
     new_data = collection.update_one({Fields.clientId.name: client_id}, 
                                      {"$push": {Fields.items.name: new_item.to_dict()}}, upsert=False)
@@ -39,16 +40,34 @@ def add_client_items(client_id):
     if new_data.modified_count == 0:
         return jsonify({"Error": "Entry not found"}), 400
     else:
-        return jsonify({"Success": "Item added", '_item_id': str(new_item.item_id)})
+        return jsonify({"Success": "Item added", '_item_id': str(new_item._item_id)})
     
 '''
 Delete a specific item for a specific client_id based on an object id
 '''
-@app.route("/entry/<client_id>/<item_id>", methods=['DELETE'])
-def delete_client_items(client_id, item_id):
+@app.route("/entry/<client_id>/<_item_id>", methods=['DELETE'])
+def delete_client_item(client_id, _item_id):
     delete_data = collection.update_one({Fields.clientId.name: client_id}, 
-                                        {"$pull": {Fields.items.name: {"_item_id": ObjectId(item_id)}}})
+                                        {"$pull": {Fields.items.name: {"_item_id": ObjectId(_item_id)}}})
     if delete_data.modified_count == 0:
         return jsonify({"Error": "Entry not found"}), 400
     else:
         return jsonify({"Success": "Item deleted"}) 
+
+''''
+Update a specific item for a specific client_id based on the object id
+'''
+@app.route("/entry/<client_id>/<_item_id>", methods=['PUT'])
+def update_client_item(client_id, _item_id):
+    request_data = request.get_json()
+    updated_item = item.Item(request_data[Fields.itemName.name], request_data[Fields.description.name],
+                            request_data[Fields.originalPhoto.name], request_data[Fields.price.name],
+                            request_data[Fields.damaged.name], request_data[Fields.damagedPhoto.name],
+                            _item_id)
+    
+    update_data = collection.update_one({Fields.clientId.name: client_id, Fields.items.name + "." + Fields._item_id.name: ObjectId(_item_id)}, 
+                                        {"$set": {Fields.items.name + ".$": updated_item.to_dict()}})
+    if update_data.modified_count == 0:
+        return jsonify({"Error": "Entry not found"}), 400
+    else:
+        return jsonify({"Success": "Item updated"})
--- objects/item.py
+++ objects/item.py
@@ -5,14 +5,14 @@ from bson.objectid import ObjectId
 An item object. This represents each household item that belongs to a client
 '''
 class Item:
-    def __init__(self, item_name, description, original_photo, price, damaged, damaged_photo):
+    def __init__(self, item_name, description, original_photo, price, damaged, damaged_photo, _item_id):
         self.item_name = item_name
         self.description = description
         self.original_photo = original_photo
         self.price = price
         self.damaged = damaged
         self.damaged_photo = damaged_photo
-        self.item_id = ObjectId()
+        self._item_id = _item_id if _item_id else ObjectId()
 
     '''
     Convert the object to a dictionary for easy serialization
@@ -25,5 +25,5 @@ class Item:
             Fields.price.name: self.price,
             Fields.damaged.name: self.damaged,
             Fields.damagedPhoto.name: self.damaged_photo,
-            Fields._item_id.name: self.item_id
+            Fields._item_id.name: self._item_id
         }
